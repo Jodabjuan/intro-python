26f06d575129d3a7aa9dc03a124f479906d7eac9
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 14:36:55 -0600

    And again and againadfasdfa

--- Words.py
+++ Words.py
@@ -6,8 +6,13 @@ Task 1: Count number of words in document
 
 from urllib.request import urlopen
 file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+count = 0
+roster = {}
 
 with urlopen(file) as story:
     for line in story:
-        
-        print(line)
+        words = line.decode('utf-8').split()
+        # print (words)
+        for word in words:
+            count += 1
+print("Total number of words: ", count)
