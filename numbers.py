95d2cab3d2446a996c0a18170bccb2e0f08936fb
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 10:45:29 -0600

    new line added

--- numbers.py
+++ numbers.py
@@ -1,4 +1,6 @@
-# print ("Hello Python")
+# print("Hello Python")
+
+
 
 # Scalar data types: Integers, Floating point
 # None, and Boolean
@@ -8,17 +10,22 @@ print("Hex notation", 0x0100)
 
 # use the int() constructor
 print(int("477"))
+
 # Option for base
-print(int("1001",2))
+print(int("1001", 2))
 
 # Floating point: IEE-754
 # Scientific notation
-print("Scientific Notation", 1.61e-35)
-print ("Float construction float()", float("1.99"))
+print("Scientific notation", 1.61e-35)
+print("Float construction float()", float("1.99"))
 # Boolean
 print("Boolean const. bool(0)", bool(0))
 print("Boolean const. bool(42)", bool(42))
 print("Boolean const. bool(0.1)", bool(0.1))
 # Test boolean on Collection
+print("Boolean const. bool([])", bool([]))
+print("Boolean const. bool([1, 2, 3])", bool([1, 2, 3]))
+print("Boolean const. bool("")", bool(""))
+print("Boolean const. bool('False')", bool("False"))
 
-
+print("New Line")
