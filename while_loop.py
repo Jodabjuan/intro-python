a3fb21f7f9209187f0e4265165de8732e5b56211
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 11:18:10 -0600

    More new code 5

--- while_loop.py
+++ while_loop.py
@@ -15,5 +15,12 @@ while counter:
     # Augmented Opperation
     counter -=1
 
+# Run forever
+while True:
+    print("Enter a number")
+    response = input()            # 4take user input
+    if int(response) % 7 == 0:    # number divisible by 7
+        break                     # exit loop
+
 
 print ("Outside while loop")
