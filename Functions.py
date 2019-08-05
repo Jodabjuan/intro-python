b27f653f0e0ca9641ef525a2b5b06ca660c50c8f
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 15:24:40 -0600

    save it all now NOW

--- Functions.py
+++ Functions.py
@@ -21,12 +21,15 @@ def main():
     Test Function
     :return:nothing
     """
-#call function
-print(__name__)
-even_or_odd(89)
-even_or_odd(44)
+    #call function
+    print(__name__)
+    even_or_odd(89)
+    even_or_odd(44)
 
 if __name__ == "__main__":
     main()
     exit(0)
 
+# load it in the console
+#import Functions (gets both even_or_odd(number) and main()
+#from Functions import even_or_odd(number) as <name> to load only one
