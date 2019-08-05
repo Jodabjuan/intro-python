5701d3a95b34c62e3cae6e640c69a280ee21f57f
CCEClass1 <CCEClass1@ad.weber.edu>
2019-08-05 10:13:46 -0600

    Initial commit

---
+++ numbers.py
@@ -0,0 +1,24 @@
+# print ("Hello Python")
+
+# Scalar data types: Integers, Floating point
+# None, and Boolean
+
+print("Binary notation", 0b0100)
+print("Hex notation", 0x0100)
+
+# use the int() constructor
+print(int("477"))
+# Option for base
+print(int("1001",2))
+
+# Floating point: IEE-754
+# Scientific notation
+print("Scientific Notation", 1.61e-35)
+print ("Float construction float()", float("1.99"))
+# Boolean
+print("Boolean const. bool(0)", bool(0))
+print("Boolean const. bool(42)", bool(42))
+print("Boolean const. bool(0.1)", bool(0.1))
+# Test boolean on Collection
+
+
