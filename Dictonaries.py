c4a7c9155036f1a06a4bbf28b28a6d06f8a3b014
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 14:00:30 -0600

    And again and again

--- Dictonaries.py
+++ Dictonaries.py
@@ -14,3 +14,16 @@ d = {'alice':'801-123-4567',
      'john': '215-564-6584'}
 print(d, type(d))
 print(d['john'])
+
+
+# Add members to the Dictionary, of names-> grades
+roster = {}
+
+counter = 0
+while counter < 3:
+    name = input("Enter a Name")
+    grade = input("Enter the Grade.")
+    roster[name] = grade
+    counter += 1
+
+print(roster)
