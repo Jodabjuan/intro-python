7df05b99e025ca1f6cc0c4b3ced9aea50bd868b6
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 10:59:56 -0600

    New code 22

--- control_flow.py
+++ control_flow.py
@@ -4,7 +4,19 @@ Learn about Control Flow in Python
 
 # IF Statement
 # Indentation is 4 spaces.
+# indents are key to the blocks.
 if True:
     print("It is true")
+
+if bool("eggs"):
+    print("Yes please!")
+
+if "eggs":
+    print("Yes, why not")
+h = 42
+if h > 50:
+    print ("Grater than 50")
+else:
+    print("Less than 50")
 
 print("Done")
