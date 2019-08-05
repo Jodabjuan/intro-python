8e4ecce1aec9fd311c74fa23f7d1ec82751214e1
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 13:24:34 -0600

    Save it now

---
+++ Lists.py
@@ -0,0 +1,26 @@
+"""
+Learn about Lists
+Unlike strings, lists are mutable
+You can update, and append elements to it
+"""
+
+# use [] for lists.
+l = [1,2,3]
+print("List ", l, type(l))
+
+
+# A list of objects (Any type)
+a = ["apple", "orange", "pear"]
+#Access by index notation
+# Replace an element
+a[0] = "tomatoes"
+a[1] = 3.445
+print(a, a[0], type(a[1]), type(a[0]))
+names = []
+counter = 0
+while counter < 3:
+    name = input("Enter a Name")
+    names.append(name)
+    counter = counter + 1
+
+print(names)
