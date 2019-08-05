ce4a349ea11d6a135d4ba1f588a144eff938fe22
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 11:13:43 -0600

    More new code 44

---
+++ while_loop.py
@@ -0,0 +1,19 @@
+""""
+Learn Conditional Repetition
+Two types of loops:  for-loops and while-loops
+"""
+
+counter = 5
+while counter !=0:
+    print(counter)
+    # Augmented Opperation
+    counter -=1
+
+counter = 5
+while counter:
+    print(counter)
+    # Augmented Opperation
+    counter -=1
+
+
+print ("Outside while loop")
