24b412f1981b2b671cd309ae30bcbcb6a7ef66b0
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-05 11:32:27 -0600

    More new code 6

---
+++ Strings.py
@@ -0,0 +1,29 @@
+"""
+Strings and Collections
+
+A string is a immutable sequence of Unicode code-points
+"""
+print ('This is a string')
+print ("This is a string too")
+print ("I can't do this")
+
+# Concatenation and adjacent Strings
+
+s1 = "First"
+s2 = "Second"
+print (s1 + " " + s2)
+
+# Multi-line Strings and newlines
+
+s3="""This is 
+a multi-line
+string"""
+print(s3)
+s4 = "This is\na multi-line\nstring"
+print(s4)
+
+# Python supports Uni-code
+# Support for backslash
+
+s5 = "A\\in a String"
+print (s5)
