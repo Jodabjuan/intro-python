3fd96a93c197a3de72e8c54a90e70b1500e6c552
jodabjuan <robertbateman@mail.Weber.edu>
2019-08-06 12:43:48 -0600

    Now working

--- refernece.py
+++ refernece.py
@@ -15,12 +15,25 @@ def modify(k):
 
 def replace(g):
     """
-    replace input list
+    replace input list, but create local copy
     :param g: input list
     :return: nothing
     """
     g =[17, 48, 89]
     print ("G = ", g)
+
+def replace_content(g):
+    '''
+    Replace the content of the input list
+    :param g: input list
+    :return: nothing
+    '''
+
+    g[0] = 88
+    g[1] = 22
+    g[2] = 44
+    print("G =  ", g)
+
 
 def main():
     """
@@ -33,6 +46,8 @@ def main():
     print("M-after = ", m)
     replace(m)
     print("M-after Replace = ", m)
+    replace_content(m)
+    print("M-after Rep_cont. = ", m)
 
 
 if __name__ == '__main__':
