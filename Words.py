64ced639dd4b5294a038d936566246eb4edc8131
jodabjuan <robertbateman@mail.Weber.edu>
2019-08-06 09:12:07 -0600

    Now working

--- Words.py
+++ Words.py
@@ -1,16 +1,19 @@
 """
 Getting information from the WEB and working with the contents
 http://icarus.cs.weber.edu/-hvalle/hafb/words.txt
+
 Task 1: Count number of words in document
 """
 
+# this is a very large library - getting only one function (import)
 from urllib.request import urlopen
 
 
 def fetch_words(filename):
     """
-    Fetch the words from a file on the WEB
-    :return: nothing
+    Count words in url file
+    :param filename: url to file
+    :return: a list of the items
     """
 
     count = 0
@@ -18,17 +21,11 @@ def fetch_words(filename):
 
     with urlopen(filename) as story:
         for line in story:
-            words = line.decode('utf-8').split()
+            words = line.decode('utf-8').split() # .split seperates the pieces using the spaces as delimiter.
             for word in words: # check to see if key already in dict.
                 data.append(word)
     return data
-               # if word in data:
-                #    data[word] += 1
-                #else:
-                 #   data[word] = 1
-                #count += 1
-   # for key in sorted(data.keys()):
-    #    print (key, data[key])
+
 
 def print_items(items):
     """
@@ -41,6 +38,10 @@ def print_items(items):
 
 
 def main():
+    """
+    Test function for words Library
+    :return: nothing
+    """
     # file = "c:/windows/default.help.txt"
     file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
     words = fetch_words(file)
