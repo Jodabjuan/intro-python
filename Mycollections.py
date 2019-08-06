e55c5dc9dce37979d9aa2671def9ab637f34cb84
jodabjuan <robertbateman@mail.Weber.edu>
2019-08-06 15:29:33 -0600

    Now working

---
+++ Mycollections.py
@@ -0,0 +1,78 @@
+"""
+Learn about collections:  Tuples, Strings, Ranges, List, Dictionaries and sets
+"""
+
+
+def do_tuples():
+    """
+    practice tuples
+    :return: nothing
+    """
+    # Immutable sequence of arbitrary objects
+    # use () to define a tuple
+    t = ("Ogden", 1.99, 2)
+    print(t, type(t))
+    print("Size ", len(t))
+    print("one object - ", t[0])
+    for item in t:
+        print(item)
+    # Single tuples, must end with a comma
+    t1 = (6, )
+    print(t1, type(t1))
+    # Tuple does not require (   )
+    t2 = 1, 2, 3, 5
+    print(t2, type(t2))
+
+
+def min_max(items):
+    """
+    returns the min and max if a collection
+    :param items: collection
+    :return: min and max
+    """
+    return min(items), max(items)
+
+
+def swap(x1, x2):
+    """
+    Swapps two values in a tuple
+    :param x1: first value to swap
+    :param x2: second value to swap
+    :return:
+    """
+    return x2, x1
+
+def do_tuple():
+    t2 = 1, 2, 3, 5
+    print(t2, type(t2))
+    
+    print(5 in (3, 6, 8, 5, 12))
+    print(5 not in (3, 6, 8, 5, 12))
+
+
+def main():
+    """
+    Test function
+    :return: 
+    """
+    # do_tuples()
+    # output = min_max([56, 79, 11, 12, 90])
+    # print("min = ", output[0])
+    # print("max = ", output[1])
+    #  tuple unpacking example
+    # lower, upper = min_max([56, 79, 11, 12, 90])
+    # print("min = ", lower)
+    # print("max = ", upper)
+    # swap values
+    # a = "jelly"
+    # b = "bean"
+    # print(a + " " + b)
+    # a, b = swap(a, b)
+    # print(a + " " + b)
+    # a, b = b, a
+    # print(a, b)
+    do_tuples()
+
+if __name__ == '__main__':
+    main()
+    exit(0)
--- scope.py
+++ scope.py
@@ -1,6 +1,29 @@
 """
-File info here
+Learn about scoping in Python
+L = local
+E = enclosed
+G = global
+B = built-in
 """
+count = 0 #global object
+
+
+def show_count():
+    """
+    Display the current count
+    :return: nothing
+    """
+    print(count)
+
+
+def set_count(n):
+    """
+    Set the count to what I want
+    :param n: input number
+    :return: nothing
+    """
+    global count
+    count = n
 
 
 def main():
@@ -8,7 +31,9 @@ def main():
     Test function
     :return: 
     """
-    pass
+    show_count()
+    set_count(9)
+    show_count()
 
 
 if __name__ == '__main__':
