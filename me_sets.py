e027e977803fe7d1e5805fb6fb4b921b9545147a
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-07 13:54:57 -0600

    Now working

--- me_sets.py
+++ me_sets.py
@@ -44,8 +44,17 @@ def main():
     blue_eyes = {"Olivia", "Harry", "Lily", "Jack"}
     blond_hair = {"Harry", "Jack", "Amelia", "Mia", "Joshua"}
     smell_hcn = {"Harry", "Amelia"}
-    taste_ptc = {"Harry", "Lily", "Ameila", "Lola"}
-    o_blood = {"Lola", "Jack", }
+    taste_ptc = {"Harry", "Lily", "Amelia", "Lola"}
+    o_blood = {"Mia", "Joshua", "Lily", "Olivia"}
+    b_blood = {"Amelia", "Jack", }
+    a_blood = {"Harry"}
+    ab_blood = {"Joshua", "Lola"}
+
+    print(blue_eyes.union(blond_hair))
+    print(blue_eyes.intersection(taste_ptc))
+    print(smell_hcn.symmetric_difference(a_blood))
+    print(blond_hair.difference(ab_blood))
+    print(taste_ptc.issuperset(smell_hcn))
 
 
 
