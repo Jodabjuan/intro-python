5f5c1f659e09ca23db37ff2c804296fa26702b15
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-07 14:26:26 -0600

    Now working

--- me_exceptions.py
+++ me_exceptions.py
@@ -17,14 +17,36 @@ def convert(s):
     return -1
 
 
+def sqrt(x):
+    """
+    Compute the square root using the method Heron of Alex
+    :param x: Number to compute the sqrt
+    :return: sqrt of x
+    """
+    guess = x
+    i = 0
+    try:
+        while guess*guess != x and i < 20:
+            guess = (guess + x/guess)/2.0
+            i += 1
+    except ZeroDivisionError:
+        raise ValueError()
+
+    return guess
+
+
+
 def main():
     """
     Test function
     :return:
     """
-    print(convert("11"))
-    print(convert("Hello"))
-    print(convert([1, 4, 8]))
+    # print(convert("11"))
+    # print(convert("Hello"))
+    # print(convert([1, 4, 8]))
+    print(sqrt(9))
+    print(sqrt(11))
+    print(sqrt(-1))
 
 
 if __name__ == '__main__':
