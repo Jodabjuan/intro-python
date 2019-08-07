762096f67a236fe4ace0bf11e4eaa381841c4bc0
Jodabjuan <robertbateman@mail.weber.edu>
2019-08-07 15:52:57 -0600

    Now working

--- iteration_protocols.py
+++ iteration_protocols.py
@@ -1,6 +1,23 @@
 """
-
+Learn about iterable, iterator objects
+Use the built-in:
+  iter(): create an iterable object
+  next(): fetch the next element in the iterable object
 """
+
+
+def first(iterable):
+    """
+    return the next member of the list IF available
+    :param iterable:  iterable object
+    :return: next member or
+    :except: ValueError for StopIteration
+    """
+    iterator = iter(iterable)
+    try:
+        return next(iterator)
+    except StopIteration:
+        raise ValueError("iterable is empty")
 
 
 def main():
@@ -8,7 +25,15 @@ def main():
     Test function
     :return:
     """
-    pass
+    iterable = ["Spring", "Summer", "Fall", "Winter"]
+    # iterator = iter(iterable)
+    # print(type(iterator), iterator)
+    # print(next(iterator))
+
+
+
+
+
 
 
 if __name__ == '__main__':
