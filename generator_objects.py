e90a8636bee84738db8497b1a6ecf5d5d54d6e0f
robertbateman <robertbateman@mail.weber.edu>
2019-08-08 10:08:44 -0600

    day 3 saves

---
+++ generator_objects.py
@@ -0,0 +1,28 @@
+"""
+Generator objects are a cross between comprehensions and generator functions
+Syntax:  similar to list comprehension, but with parenthesis:
+    (expr(item) for item in iterable)
+"""
+
+from list_comprehensions import is_prime
+
+
+def main():
+    """
+    Test function
+    :return:
+    """
+    # list with first 1 million square numbers
+    m_sq = (x*x for x in range(1, 1000001))
+    print(m_sq, type(m_sq))
+    print('The sum of the first 1 million square numbers is: ', sum(m_sq))
+    print('The sum of the first 1 million square numbers is: ',
+          sum(x*x for x in range(1, 1000001)))
+    # the sum of the prime numbers between 1 and 10K
+    print('The sum of the prime numbers from 1 to 10K: ',
+          sum(x for x in range(1, 10001) if is_prime(x)))
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
