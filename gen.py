227b11eca438e100877e9bef4236b6fe5a689cb3
robertbateman <robertbateman@mail.weber.edu>
2019-08-08 09:21:08 -0600

    day 3 saves

--- gen.py
+++ gen.py
@@ -22,13 +22,33 @@ def run_take():
     """
     Test the take() function
     """
-    items = {2, 4, 6, 8, 10}
+    items = [2, 4, 6, 8, 10]
     for item in take(3, items):
         print(item)
 
 
+def distinct(iterable):
+    """
+    Return unique items by eliminating duplicates
+    :param iterable: The source series
+    :yields: Unique elements in order from 'iterable'
+    """
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        yield item
+        seen.add(item)
+
+
+def run_distinct():
+    items = [4, 5, 6, 77, 2, 4, 1, 8, 5]
+    for item in distinct(items):
+        print(item)
+
+
 def main():
-    run_take()
+    run_distinct()
 
 
 if __name__ == '__main__':
