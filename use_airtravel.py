e784e43e5d574b78ef4c8d41def14a46dac42eaf
robertbateman <robertbateman@mail.weber.edu>
2019-08-08 14:47:26 -0600

    day 3 saves

--- use_airtravel.py
+++ use_airtravel.py
@@ -9,15 +9,12 @@ def main():
     Test function
     :return:
     """
-    f1 = Flight("SN066")
-    print(f1.number(), f1.airline())  # could use: Flight.number(f)  :(  :(
-    f2 = Flight("FA044")
-    print(f2.number(), f2.airline())
-
-    a1 = Aircraft("G-EUP", "Airbus A319",
-                  numrows=22, seatsper=6)
-    print(a1.registration(), a1.model())
-    pp(a1._seating)
+    f1 = Flight("SN066",
+                Aircraft("G-EUP",
+                         "Airbus A319",
+                         numrows=22,
+                         seatsper=6))
+    pp(f1._seating)
 
 
 if __name__ == '__main__':
